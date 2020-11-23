81fd2ec74b22213aa51cc612fa70c81c6a7feb1e
fdiazdev <61359587+fdiazdev@users.noreply.github.com>
2020-11-23 13:38:59 -0300

    carga inicial de proyecto
    JDC

---
+++ hall.php
@@ -0,0 +1,86 @@
+<?php
+require_once('php/funciones_base.php');
+?>
+<br>
+<div class="container">
+<h1>Example heading <span class="badge badge-secondary">New</span></h1>
+</div>
+<br><br>
+<div class="container">
+  <div class="row">
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="img/hall1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">Categoría 1</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="img/hall1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">Categoría 2</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="img/hall1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">Categoría 3</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    </div>
+  </div>
+  <br><br>
+  <div class="row">
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="img/hall1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">Categoría 4</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="img/hall1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">Categoría 5</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="img/hall1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                      <h5 class="card-title">Categoría 6</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    </div>
+
+
+  </div>
+
+</div>
+<br><br>
+<?php
+require_once('php/footer.php');
+?>
